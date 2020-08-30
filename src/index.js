a6cc5f3cccbff2087d20ae08912ca685dd46a7a2
KJFreys1 <kfreyermuth20@gmail.com>
2020-08-30 15:20:52 -0700

    landing page starter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+// import App from './Dashboard';
+import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router><App /></Router>,
   document.getElementById('root')
 );
